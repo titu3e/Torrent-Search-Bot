0e787ca943d56c62e56fc4b029d74f687cf8f654
titu3e <75441242+titu3e@users.noreply.github.com>
2021-11-09 15:56:08 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -5,7 +5,7 @@ import os
 
 class Config(object):
     SESSION_NAME = os.environ.get("SESSION_NAME")
-    API_ID = int(os.environ.get("API_ID", 12345678))
-    API_HASH = os.environ.get("API_HASH")
-    BOT_TOKEN = os.environ.get("BOT_TOKEN")
+    API_ID = int(os.environ.get("8626831", 8626831))
+    API_HASH = os.environ.get("db23330a6edf4a517ee186b35cedec71")
+    BOT_TOKEN = os.environ.get("2111558373:AAEJpYwWbrzMXjCvfHdix7m2imhXsywF57Y")
     MAX_INLINE_RESULTS = int(os.environ.get("MAX_INLINE_RESULTS", 50))
